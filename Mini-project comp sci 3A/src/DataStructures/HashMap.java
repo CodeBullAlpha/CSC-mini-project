aba11a004de28330fe8130179d7ca02862731416
tumi <a@b.c>
2025-05-19 12:27:33 +0200

    Documented HashMap

--- Mini-project comp sci 3A/src/DataStructures/HashMap.java
+++ Mini-project comp sci 3A/src/DataStructures/HashMap.java
@@ -4,10 +4,27 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class HashMap<K, V> {
+
+    /*
+     * Current number of items in the HashMap.
+    */
     private int size;
+
+    /*
+     * Fixed capacity of the HashMap's underlying bucket array.
+    */
     private int capacity;
+
+    /*
+     * Integers used in computing a key's hash.
+    */    
     private int prime;
     private int scale, shift;
+
+
+    /*
+     * The underlying bucket array used to store items.
+    */    
     private SLinkedList<V>[] bucketArray;
 
     public HashMap() {
@@ -22,15 +39,24 @@ public class HashMap<K, V> {
 
 	bucketArray = (SLinkedList<V>[])new SLinkedList[capacity];
     }
-    
+
+    /*
+     * @return current number of items in the HashMap.
+     */
     public int size() {
 	return size;
     }
 
+    /*
+     * @return a boolean of whether or not the hashmap has items.
+     */
     public boolean isEmpty() {
 	return size == 0;
     }
 
+    /* @param key supplied key
+     * @return value associated with the supplied key
+     */
     public V get(K key){
 	int index = hash(key);
 
@@ -40,19 +66,30 @@ public class HashMap<K, V> {
 	return bucketArray[index].first();
     }
 
+    /*
+     * Associates a key with a value and returns the value.
+     * @param key supplied key.
+     * @param value supplied value to associate with key.
+     * @return value associated with key 
+     */
     public V put(K key, V value) {
 	int index = hash(key);
 	
 	if(bucketArray[index] == null) {
 	    bucketArray[index] = new SLinkedList<V>();
 	}
-
+	
 	bucketArray[index].addFirst(value);
 	size++;
 
 	return value;
     }
 
+    /*
+     * Removes the first value associated with supplied key and returns it.
+     * @param key
+     * @return old value associated with supplied key.
+     */
     public V remove(K key) {
 	int index = hash(key);
 	
